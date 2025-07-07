56a9ee9aad3c5b8dfbf37c6cd556253fc27e6f99
roberto <roberdex26@gmail.com>
2025-07-06 20:57:45 -0600

    06/07/2025
    terminando el modulo 4 de astro, routing dinamico

---
+++ src/components/react/Toolbar.jsx
@@ -0,0 +1,11 @@
+export function Toolbar() {
+  return (
+    <div className="toolbar">
+      <p>
+        asdas asd asdas das dasd asd das das 
+        <aside>dasdas dasdasdasdasdasdasd</aside>
+        dsadasdasdd d sadas dsada  dsaddasdasda a
+      </p>
+    </div>
+  );
+}
